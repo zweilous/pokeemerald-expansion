48520785cd7d862a366747e3e41689a5f39ea8c2
sid <moltres@outlook.com>
2026-04-07 18:57:08 -0400

    src/title_screen.c

--- src/title_screen.c
+++ src/title_screen.c
@@ -487,12 +487,14 @@ static void SpriteCB_PokemonLogoShine(struct Sprite *sprite)
 
             // Flash the background green for 4 frames of movement.
             // Otherwise use the updating color.
+            /*
             if (sprite->x == DISPLAY_WIDTH / 2 + (3 * SHINE_SPEED)
              || sprite->x == DISPLAY_WIDTH / 2 + (4 * SHINE_SPEED)
              || sprite->x == DISPLAY_WIDTH / 2 + (5 * SHINE_SPEED)
              || sprite->x == DISPLAY_WIDTH / 2 + (6 * SHINE_SPEED))
                 gPlttBufferFaded[0] = RGB(24, 31, 12);
             else
+            */
                 gPlttBufferFaded[0] = backgroundColor;
         }
 
