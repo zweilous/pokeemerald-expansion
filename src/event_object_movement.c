67929009404815e4e502a962afca2fd8a3451fae
sid <moltres@outlook.com>
2026-04-19 16:10:02 -0400

    reintroduced follower pkmn restrictions

--- src/event_object_movement.c
+++ src/event_object_movement.c
@@ -2169,6 +2169,15 @@ struct Pokemon *GetFirstLiveMon(void)
     {
         for (i = 0; i < PARTY_SIZE; i++)
         {
+            struct Pokemon *mon = &gPlayerParty[i];
+            u32 species = GetMonData(mon, MON_DATA_SPECIES_OR_EGG);
+            if (species == SPECIES_NONE)
+                continue;
+
+            if ((OW_FOLLOWERS_ALLOWED_SPECIES && species != VarGet(OW_FOLLOWERS_ALLOWED_SPECIES))
+            || (OW_FOLLOWERS_ALLOWED_MET_LVL && GetMonData(mon, MON_DATA_MET_LEVEL) != VarGet(OW_FOLLOWERS_ALLOWED_MET_LVL))
+            || (OW_FOLLOWERS_ALLOWED_MET_LOC && GetMonData(mon, MON_DATA_MET_LOCATION) != VarGet(OW_FOLLOWERS_ALLOWED_MET_LOC)))
+                continue;
             if (gPlayerParty[i].hp > 0 && !(gPlayerParty[i].box.isEgg || gPlayerParty[i].box.isBadEgg))
                 return &gPlayerParty[i];
         }
@@ -2180,6 +2189,15 @@ struct Pokemon *GetFirstLiveMon(void)
     {
         for (i = 0; i < PARTY_SIZE; i++)
         {
+            struct Pokemon *mon = &gPlayerParty[i];
+            u32 species = GetMonData(mon, MON_DATA_SPECIES_OR_EGG);
+            if (species == SPECIES_NONE)
+                continue;
+
+            if ((OW_FOLLOWERS_ALLOWED_SPECIES && species != VarGet(OW_FOLLOWERS_ALLOWED_SPECIES))
+            || (OW_FOLLOWERS_ALLOWED_MET_LVL && GetMonData(mon, MON_DATA_MET_LEVEL) != VarGet(OW_FOLLOWERS_ALLOWED_MET_LVL))
+            || (OW_FOLLOWERS_ALLOWED_MET_LOC && GetMonData(mon, MON_DATA_MET_LOCATION) != VarGet(OW_FOLLOWERS_ALLOWED_MET_LOC)))
+                continue;
             if (gPlayerParty[i].hp > 0 && !(gPlayerParty[i].box.isEgg || gPlayerParty[i].box.isBadEgg))
                 return &gPlayerParty[i];
         }
